2f312e00c2f7526aef7f7e8ae69ca4176bbe1878
irsathhazar <irsathhazar@gmail.com>
2025-03-29 06:08:42 +0530

    Update wsgi.py
    Instead of Restapi - used laysans_backend

--- laysans_backend/wsgi.py
+++ laysans_backend/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Restapi project.
+WSGI config for laysans_backend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Restapi.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'laysans_backend.settings')
 
 application = get_wsgi_application()
