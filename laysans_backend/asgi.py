2e835263c7f03e3637a12f715280c50e39fa73a1
irsathhazar <irsathhazar@gmail.com>
2025-03-29 06:07:13 +0530

    Update asgi.py
    Instead of Restapi - used laysans_backend

--- laysans_backend/asgi.py
+++ laysans_backend/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Restapi project.
+ASGI config for laysans_backend project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Restapi.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'laysans_backend.settings')
 
 application = get_asgi_application()
